d096bd2ef0c339d16fd1fe0c53538b8a5cde88f0
JamesSchiller <schillerj78@gmail.com>
2020-06-14 02:55:47 -0400

    new header and footer layout

--- services/mail.js
+++ services/mail.js
@@ -88,7 +88,22 @@ module.exports = {
     //     //         errorEmails,
     //     //     }
     //     // );
-    // }, 
+    // },
+    
+// Need "Welcome to Jinnmail" email sent upon successful new user activation (when user validates email with code after signup). 
+// FROM: Jinnmail <hello@jinnmail.com>
+// REPLY-TO: Jinnmail Support <help@jinnmail.com>
+// BODY: Welcome to Jinnmail, the tool that keeps your email address private and spam-free by using aliases that mask your real email address. 
+
+// A message sent by anyone to one of your aliases will arrive in this [[UserEmail]] inbox like normal, but when you reply it is sent by your alias; the recipient never knows your real address. We recommend generating a new alias for every new form. Or create a custom alias at your "Account Dashboard" <https://jinnmail.com/account> to use for mobile apps. (Mobile support soon). Emails are never stored or read by any human beings.
+
+// Generate new email aliases when signing up or filling out any webform using the "Jinnmail extension in Chrome" <https://go.jinnmail.com/chrome>. Clicking the small Jinnmail button that appears in any email address box creates and inserts a new alias into the webform.
+
+// You can deactivate/reactivate any aliases from the Jinnmail browser extension, or you can delete your aliases at your "Account Dashboard" <https://jinnmail.com/account>.
+
+// This is a beta and there could be bugs. We want to make this software something you use everyday, so please reach out to help@jinnmail.com with any comments, problems, or suggestions. 
+
+// Thanks for staying private using Jinnmail!
 
     send_mail: function(params) {
         var {to, from, replyTo, subject, cc, headers, messageBody, attachments} = params
