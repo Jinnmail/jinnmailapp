5d6715f35b1ea8b49a78d27f1da05d3e1783fa1d
James Schiiller <schillerj78@gmail.com>
2021-12-25 17:57:49 -0500

    delete proxymail

--- controller/proxymail.js
+++ controller/proxymail.js
@@ -18,5 +18,24 @@ module.exports = {
       error.result = "";
       res.status(err.status).send(error);
     }
+  }, 
+
+  deleteProxymail: async function(req, res) {
+    try {
+      const data = await proxymailModel.deleteOne({ senderAliasId: req.params.aliasId })
+      let response = {
+        status: 200,
+        message: 'deleted successfully',
+        data: data,
+        error: ''
+      };
+      res.status(200).send(response);
+    } catch (err) {
+      let error = {};
+      error.status = 500;
+      error.error = err.msg;
+      error.result = "";
+      res.status(err.status).send(error);
+    }
   }
 }
--- routes/proxymail.js
+++ routes/proxymail.js
@@ -4,5 +4,6 @@ var proxymail = require('../controller/proxymail.js');
 const userAuth = require('../middlewares/userAuth');
 
 router.get('/:aliasId', userAuth.validateUser, proxymail.getProxymail);
+router.delete('/:aliasId', userAuth.validateUser, proxymail.deleteProxymail);
 
 module.exports = router;
